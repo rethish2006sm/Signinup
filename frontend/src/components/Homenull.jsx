6ceb1e1912dbb7717484b6555082aa3aaf1a458a
Rethish Mudaliar <rethish.2006sm@gmail.com>
2026-07-30 19:10:14 +0530

    Fix logo text from 'Start-upP' to 'Start-up'

--- frontend/src/components/Homenull.jsx
+++ frontend/src/components/Homenull.jsx
@@ -12,7 +12,7 @@ const Homenull = () => {
 
           {/* Logo */}
           <div className="text-5xl font-bold">
-            Start-upP
+            Start-up
           </div>
 
           {/* Menu */}
