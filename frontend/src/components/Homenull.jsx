96e76939a7830c453ddeead8985601916b15f1ee
Rethish Mudaliar <rethish.2006sm@gmail.com>
2026-08-05 22:01:16 +0530

    Change logo text

--- frontend/src/components/Homenull.jsx
+++ frontend/src/components/Homenull.jsx
@@ -12,7 +12,7 @@ const Homenull = () => {
 
           {/* Logo */}
           <div className="text-5xl font-bold">
-            Start-IP
+            Start-UP
           </div>
 
           {/* Menu */}
