d158da82f357502c0a9f2aabc4877ff9a9cee5c0
Rethish Mudaliar <rethish.2006sm@gmail.com>
2026-08-06 18:46:26 +0530

    Update Signin.jsx

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -39,7 +39,7 @@ const Signin = () => {
       <div className="bg-white p-8 rounded-2xl shadow-2xl w-[360px]">
         
         <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">
-          Signin
+          SignIN
         </h1>
 
         <input
