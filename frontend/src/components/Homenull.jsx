1a09be20ae5d08bf54a7e3e24b3da20981dc6879
Rethish Mudaliar <rethish.2006sm@gmail.com>
2026-09-24 00:05:47 +0530

    Change 'STARTUP' to 'startup' in Homenull.jsx

--- frontend/src/components/Homenull.jsx
+++ frontend/src/components/Homenull.jsx
@@ -12,7 +12,7 @@ const Homenull = () => {
 
           {/* Logo */}
           <div className="text-5xl font-bold">
-            STARTUP
+            startup
           </div>
 
           {/* Menu */}
